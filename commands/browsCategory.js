d07f0fc5fbda1e89cf0f39c20224d2a959251405
Amin.A <amin@goquo.com>
2017-10-10 18:50:13 +0800

    fix browsCat

--- commands/browsCategory.js
+++ commands/browsCategory.js
@@ -6,7 +6,6 @@ let Command = require('../libs/commands/Command');
 let image64 = require('../libs/utils/image64');
 const {Extra, memorySession} = require('telegraf');
 module.exports = new Command('middleware', 'browsCategory', (ctx) => {
-    //ctx.session.value = 0
     if (ctx.session.state == 'shop') {
         Categories
             .findById(JSON.parse(ctx.state.data).catID)
@@ -19,40 +18,34 @@ module.exports = new Command('middleware', 'browsCategory', (ctx) => {
                         ctx.session.pagination = 0;
                         ctx.session.maxPage = products.length - 1;
                         const markup = Extra
-                            .HTML('hello')
+                            .HTML()
                             .markup((m) => m.inlineKeyboard([
-                                    m.callbackButton("Back", 'browsCategory#:#back'),
-                                    m.callbackButton("Next", 'browsCategory#:#next')
+                                    [m.callbackButton("Back", 'browsCategory#:#back'),
+                                        m.callbackButton("Next", 'browsCategory#:#next')],
+                                    [m.callbackButton("Order", 'browsCategory#:#order')]
                                 ]
                                 , {columns: 2}))
-
-                        const base64Img = require('base64-img');
-                        const crypto = require('crypto');
-
-                        let name = crypto.createHash('md5').update(new Date().toUTCString()).digest("hex");
-                        let image = base64Img.imgSync(products[0].image, './tmp/', name);
-                        ctx
-                            .replyWithPhoto({source: require('path').resolve(__dirname, '../' + image)}, markup)
-                            .then(() => undefined)
-                            .catch((err) => {
-                                console.log(err)
-                            });
-
-                        /*
-                         image
-                         title + price count
-                         */
-                        /*let product = products[ctx.session.pagination];
-                         let messageBody = (`<b>${product.title}</b>` ) +
-                         ( product.image !== null && products.image !== undefined ? `<imgage src="${product.image}"></imgage>` : ``) +
-
-                         `<b>Price: ${product.price}</b>` +
-                         `<b>Available : ${product.count !== 0}</b>`;
-
-                         ctx.editMessageReplyMarkup(messageBody, markup).catch((err) => {
-                         console.log(err)
-                         });*/
-
+                        let product = products[ctx.session.pagination];
+                        let messageBody = (`${product.title}\n\r` ) +
+                        `Price: ${product.price}\n\r` +
+                        product.count == 0 ? `out of stock\n\r` : `in stock : ${product.count} left\n\r`;
+                        if (product.image == undefined || product.image == null) {
+                            // send res by text only
+                            ctx.reply(messageBody, markup)
+                                .then(() => undefined)
+                                .catch(console.log);
+                        }
+                        else {
+                            /*
+                             image
+                             title + price count
+                             */
+                            markup.caption = messageBody;
+                            ctx
+                                .replyWithPhoto({source: require('../libs/utils/image64').decodeImage(product.image).buffer}, markup)
+                                .then(() => undefined)
+                                .catch(console.log);
+                        }
                     })
             })
             .catch((err) => {
